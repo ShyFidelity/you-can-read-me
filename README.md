4471c5c9de9846136d038b74fcf996f1dc90b7eb
jkworks <info@jilliankayworks.com>
2021-08-24 12:03:39 -0600

    working

--- README.md
+++ README.md
@@ -1,25 +1,25 @@
-# s
-  (https://img.shields.io/badge/license-ApacheLicense2.0-blue.svg)
+# 
+  ![](https://img.shields.io/badge/license-ApacheLicense2.0-blue.svg)
 ## GitHub Username and link
-##[s](https://github.com/s/)
+##[](https://github.com//)
 ## Table of Contents
 * [Description](#Description) 
-  [Motivation](#motivation)
-  [Installation](#Installation)
-  [How to Use](#usage)
-  [Contributors](#contributors) 
-  [Test:](#test)
-  [Dependencies:](#dependencies)
+* [Motivation](#motivation)
+* [Installation](#Installation)
+* [How to Use](#usage)
+* [Contributors](#contributors) 
+* [Test:](#test)
+* [Dependencies:](#dependencies)
   
 
 ## Description 
-  s
+  
 ## Motivation 
   
 ## Installation
   npm i
 ## How to use 
-  s
+  
 ## Contributors
   
 ## Test
@@ -28,5 +28,5 @@
   npm i
 ## License
 
-    Copyright: ApacheLicense2.0
-    [License link: ]undefined
+Copyright: ApacheLicense2.0
+[License link](https://www.apache.org/licenses/LICENSE-2.0)
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,37 +4,44 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
  if (license !== "None") {
-   return `(https://img.shields.io/badge/license-${license}-blue.svg)`;
+   return `![](https://img.shields.io/badge/license-${license}-blue.svg)`;
  } return ' ';
 
 }
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink() {
+function renderLicenseLink(license) {
   let lApache = '(https://www.apache.org/licenses/LICENSE-2.0)';
   let lMIT = '(https://opensource.org/licenses/MIT)';
   let lMozilla = '(https://www.mozilla.org/en-US/MPL/2.0/)';
   let lArtistic = '(https://opensource.org/licenses/Artistic-2.0)';
   let LOliva = '(https://www.youtube.com/watch?v=ZmDBbnmKpqQ)';
 
-  let linkArray = [lApache,lMIT,lMozilla,lArtistic,LOliva];
-
-  for (const value of linkArray) {
-  console.log(value);
-  
-    
+  // let linkArray = [lApache,lMIT,lMozilla,lArtistic,LOliva];
+  switch (license){
+    case 'ApacheLicense2.0': 
+    return lApache
+    case 'MIT':
+    return lMIT
+    case 'MozillaPublicLicense2.0':
+    return lMozilla
+    case 'ArtisticLicense2.0':
+    return lArtistic
+    case 'OliviaRodrigoDriversLicense':
+    return LOliva;
+    default:
+    return '';
   }
-  
 
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(value) {
+function renderLicenseSection(license) {
   if (license !== "None") {
     return `
-    Copyright: ${license}
-    [License link: ]${renderLicenseLink(value)}`;
+Copyright: ${license}
+[License link]${renderLicenseLink(license)}`;
     
   }
 }
@@ -50,12 +57,12 @@ function generateMarkdown(data) {
 ##[${data.username}](https://github.com/${data.username}/)
 ## Table of Contents
 * [Description](#Description) 
-  [Motivation](#motivation)
-  [Installation](#Installation)
-  [How to Use](#usage)
-  [Contributors](#contributors) 
-  [Test:](#test)
-  [Dependencies:](#dependencies)
+* [Motivation](#motivation)
+* [Installation](#Installation)
+* [How to Use](#usage)
+* [Contributors](#contributors) 
+* [Test:](#test)
+* [Dependencies:](#dependencies)
   
 
 ## Description 
@@ -73,7 +80,7 @@ function generateMarkdown(data) {
 ## Dependencies
   ${data.dependencies}
 ## License
-${renderLicenseSection(value[i])}
+${renderLicenseSection(data.license)}
 `;
 
 
